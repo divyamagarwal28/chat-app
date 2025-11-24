908b220127b8e99bb487d3ca3e0236546d4fbc4f
divyamagarwal28 <divyam.agarwal@tetr.org>
2025-11-24 17:16:10 +0530

    Fix: allow sender to receive messages

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 const port = process.env.PORT || 8000;
-
 const io = require("socket.io")(port, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
-const users = {};
+const users = {}; // store connected users
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+  console.log("User connected:", socket.id);
 
-    socket.on("user-joined", (name) => {
-        users[socket.id] = name;
+  // User joins chat
+  socket.on("user-joined", (name) => {
+    users[socket.id] = name;
 
-        // Send SYSTEM message to everyone including me
-        io.emit("receive", {
-            name: "System",
-            message: `${name} joined the chat`
-        });
+    // Notify everyone except the new user
+    socket.broadcast.emit("receive", {
+      name: "System",
+      message: `${name} joined the chat`
     });
 
-    socket.on("send", (message) => {
-
-        // Send message to everyone including me
-        io.emit("receive", {
-            message,
-            name: users[socket.id]
-        });
+    // Optional: Send welcome message to the new user
+    socket.emit("receive", {
+      name: "System",
+      message: `Welcome ${name}!`
     });
+  });
 
-    socket.on("disconnect", () => {
-        delete users[socket.id];
-        console.log("User disconnected:", socket.id);
+  // User sends a message
+  socket.on("send", (message) => {
+    // Send to everyone except sender
+    socket.broadcast.emit("receive", {
+      message,
+      name: users[socket.id]
     });
+  });
+
+  // User disconnects
+  socket.on("disconnect", () => {
+    const name = users[socket.id];
+    if (name) {
+      // Notify everyone that user left
+      socket.broadcast.emit("receive", {
+        name: "System",
+        message: `${name} left the chat`
+      });
+      delete users[socket.id];
+    }
+    console.log("User disconnected:", socket.id);
+  });
 });
